117c392c17559675691a65d0b2deba8a6e7b9a73
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2023-08-14 13:25:26 +0100

    add artist, song and related entities

---
+++ Entities/Artist.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SamCooks.API.Entities
+{
+    public class Artist : BaseEntity
+    {
+    }
+}
