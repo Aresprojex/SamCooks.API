603c4602cab6a300e3afd3dd511804b13b6bdee0
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2023-08-14 18:06:33 +0100

    add base controller and artist controller

---
+++ Controllers/ArtistController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SamCooks.API.DTO;
+using SamCooks.API.Helpers;
+using SamCooks.API.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SamCooks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArtistController : BaseController
+    {
+        [HttpGet]
+        [ProducesResponseType(typeof(BaseResponse<GetArtists>), 200)]
+        public async Task<IActionResult> Artist([FromQuery] BaseSearchViewModel model)
+        {
+
+            try
+            {
+                return ReturnResponse(await _cardService.GetArstis(model));
+            }
+            catch (Exception ex)
+            {
+                return HandleError(ex);
+            }
+        }
+    }
+}
---
+++ Controllers/BaseController.cs
@@ -0,0 +1,62 @@
+﻿using log4net;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SamCooks.API.Enum;
+using SamCooks.API.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+
+namespace SamCooks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        private readonly ILog _logger;
+        public BaseController()
+        {
+            _logger = LogManager.GetLogger(typeof(BaseController));
+        }
+        protected IActionResult ReturnResponse(dynamic model)
+        {
+            if (model.Status == RequestExecution.Successful)
+            {
+                return Ok(model);
+            }
+
+            return BadRequest(model);
+        }
+
+        protected Guid UserId
+        {
+            get { return Guid.Parse(User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value); }
+        }
+
+        protected DateTime CurrentDateTime
+        {
+            get
+            {
+                return DateTime.UtcNow;
+            }
+        }
+
+        protected IActionResult HandleError(Exception ex, string customErrorMessage = null)
+        {
+            _logger.Error(ex.Message, ex);
+            _logger.Error(ex.StackTrace, ex);
+
+
+            BaseResponse<string> rsp = new BaseResponse<string>();
+            rsp.Status = RequestExecution.Error;
+#if DEBUG
+            rsp.Errors = new List<string>() { $"Error: {(ex?.InnerException?.Message ?? ex.Message)} --> {ex?.StackTrace}" };
+            return Ok(rsp);
+#else
+            rsp.Errors = new List<string>() { "An error occurred while processing your request!" };
+            return BadRequest(rsp);
+#endif
+        }
+    }
+}
