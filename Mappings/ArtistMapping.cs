d2fb1d205fe9c695111129bae81bbf5d7f7862e8
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2023-08-15 13:46:52 +0100

    add default mapping profiles

---
+++ Mappings/ArtistMapping.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SamCooks.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SamCooks.API.Mappings
+{
+    public class ArtistMapping : IEntityTypeConfiguration<Artist>
+    {
+        public void Configure(EntityTypeBuilder<Artist> builder)
+        {
+            var artists = new List<Artist>
+            {
+                new Artist
+                {
+                    Id = Guid.Parse("e7d58c75-18bc-4868-b54d-0a1fdf8fe94d"),
+                    Name = "21 Savage",
+                    Bio = "21 Savage is a British rapper known for his <i>distinct trap style</i> and introspective lyrics. Born in London, he moved to Atlanta at a young age and emerged as a prominent figure in the hip-hop scene. His music often reflects on his personal experiences, addressing topics like <i>violence</i>, <i>poverty</i>, and <b><span style='color: red';>social injustice</span></b>.",
+                    Region = "South",
+                    Location = "Atlanta, Georgia"
+                },
+            };
+
+            builder.HasData(artists);
+        }
+    }
+}
---
+++ Mappings/SongMapping.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SamCooks.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SamCooks.API.Mappings
+{
+    public class SongMapping : IEntityTypeConfiguration<Song>
+    {
+        public void Configure(EntityTypeBuilder<Song> builder)
+        {
+            var songs = new List<Song>();
+
+            songs.Add(new Song
+            {
+                Name = "A Lot",
+                ArtistId = Guid.Parse("e7d58c75-18bc-4868-b54d-0a1fdf8fe94d"), // 21 Savage
+                SociallyConscious = Enum.SociallyConscious.Yes,
+                CulturallySignificant = Enum.CulturallySignificant.Yes,
+                ReasonOfSelection = "This song addresses various social issues and personal experiences, including the effects of violence, racism, and the struggle for success.",
+                Keywords = new List<Keyword>()
+                {
+                    new Keyword
+                    {
+                        Id = Guid.NewGuid(),
+                        Word = "#Violence",
+                    },
+                    new Keyword
+                    {
+                        Id = Guid.NewGuid(),
+                        Word = "#Success",
+                    },
+                    new Keyword
+                    {
+                        Id = Guid.NewGuid(),
+                        Word = "#Loyalty",
+                    },
+                    new Keyword
+                    {
+                        Id = Guid.NewGuid(),
+                        Word = "#LifeOverWealthOrDeath",
+                    },
+                    new Keyword
+                    {
+                        Id = Guid.NewGuid(),
+                        Word = "#Struggles",
+                    },
+                    new Keyword
+                    {
+                        Id = Guid.NewGuid(),
+                        Word = "#Betrayal",
+                    },
+                    new Keyword
+                    {
+                        Id = Guid.NewGuid(),
+                        Word = "#GunViolence",
+                    }
+                },
+                OverallTheme = "Reflection",
+                LinkedLyrics = "https://genius.com/21-savage-a-lot-lyrics"
+            });
+
+            builder.HasData(songs);
+        }
+    }
+}
